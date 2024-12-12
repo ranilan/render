2fa2b3453250aaf64e45292af9810b802ee981c4
ranilan <ranilan00@gmail.com>
2024-12-12 14:38:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+# Route לדף הראשי
 @app.route('/')
 def home():
     return render_template("index.html")
+
+# API שיחזיר נתונים על משתמש
+@app.route('/api/user')
+def get_user():
+    user = {
+        'name': 'John Doe',
+        'age': 30,
+        'email': 'john.doe@example.com'
+    }
+    return jsonify(user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
