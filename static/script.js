a9eef963ca7de45b7eed35c5e24385bbbb835c3c
ranilan <ranilan00@gmail.com>
2024-12-12 14:53:04 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,16 +1,13 @@
-document.getElementById("changeTextButton").addEventListener("click", function () {
-  const greeting = document.getElementById("greeting");
-  greeting.textContent = "You clicked the button! 🎉";
-  greeting.style.color = "#28a745";
-});
-
-// בקשה ל-API לקבלת מידע על משתמש
 document.getElementById("getUserButton").addEventListener("click", function () {
   fetch('/api/user')
     .then(response => response.json())
     .then(data => {
       const userInfo = document.getElementById("userInfo");
-      userInfo.textContent = `Name: ${data.name}, Age: ${data.age}, Email: ${data.email}`;
+      if (data.message) {
+        userInfo.textContent = data.message;
+      } else {
+        userInfo.textContent = `Name: ${data.name}, Age: ${data.age}, Email: ${data.email}`;
+      }
       userInfo.style.color = "#007bff";
     })
     .catch(error => console.error('Error fetching user data:', error));
